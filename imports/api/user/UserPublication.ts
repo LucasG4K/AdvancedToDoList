14e2746349049b55e6112fc66be7d2a6478e3c1f
Lucas Gontijo <83668006+LucasG4K@users.noreply.github.com>
2025-03-21 15:18:19 -0300

    corrigindo bug pasta user duplicada no GitHub

--- imports/api/user/UserPublication.ts
+++
@@ -1,8 +0,0 @@
-// Publica dados do usuário autenticado
-
-import { Meteor } from "meteor/meteor";
-
-Meteor.publish("users", function () {
-  if (!this.userId) throw new Meteor.Error("not-authorized");
-  return Meteor.users.find();
-});
